ecf8433214efd2baba92c49f8507c7fb777ffc1c
Sergey Pie <sergeymurashko007@gmail.com>
2017-09-08 16:09:28 +0300

    Setup environment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+var App = React.createClass({
+
+	render: function() {
+		return (
+			<h1>qwerty</h1>
+		);
+	}
+
+});
+
+export default App;
---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = React.createClass({
+    render() {
+        return (
+            <div className='App'>
+               ghjgj
+            </div>
+        );
+    }
+});
+
+export default App;
