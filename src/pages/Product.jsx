d523d7cc04faeb84088d54898094f34bccbc5c9f
Sergey Pie <sergeymurashko007@gmail.com>
2017-09-10 23:59:05 +0300

    product markup

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -11,16 +11,24 @@ function Product(){
 		return (
 			
 		        <div className="product">
-		            <div className="col-lg-6 col-md-6 ">
-		              <div className="product__text">
-		              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque eum corporis nihil impedit autem numquam enim sapiente, iusto iure magnam dolores ea sunt unde dolore veniam, facilis temporibus blanditiis nemo.
-		              </div>
-		            </div>
-		            <div className="col-lg-7 col-md-7 absolute">
+		        <div className="col-lg-offset-5 col-md-offset-5 col-sm-offset-5 lol col-lg-7  col-md-7 col-sm-7">
 		              <div className="product__image">
 		                <img src="img/3.jpg" alt=""/>
 		              </div>
 		          </div>
+		            <div className="col-lg-6 col-md-6 col-sm-6 qwe ">
+		              <div className="product__text">
+		              	<h1>London Sandwich</h1>
+		              	<p>loremLorem ipsum dolor sit amet, consectetur adipisicing elit. At qui doloribus perferendis eaque reiciendis repellat porro, sapiente maxime in, eos commodi laborum cupiditate fugiat debitis facere deleniti alias minus dignissimos.</p>
+		                
+						<button className="card-button">
+							<span className="lnr lnr-cart"></span>
+							<span className="card-button__text">Add to cart</span>
+						</button>
+		              </div>
+
+		            </div>
+		            
        		 </div> 
        
 		);
